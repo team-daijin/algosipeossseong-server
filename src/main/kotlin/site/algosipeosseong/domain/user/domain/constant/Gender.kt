a7a93f44bd534ffbfc83a65f899c86cb66d4f186
pea06 <jowangyu75@dgsw.hs.kr>
2024-04-07 20:15:47 +0900

    add user gender constant

---
+++ src/main/kotlin/site/algosipeosseong/domain/user/domain/constant/Gender.kt
@@ -0,0 +1,9 @@
+package site.algosipeosseong.domain.user.domain.constant
+
+enum class Gender(
+    val value: String,
+    val kr: String
+) {
+    MALE("MALE", "남자"),
+    FEMALE("FEMALE", "여자")
+}

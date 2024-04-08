1984c5816972c1121250e68b03d5cd0b177f584c
pea06 <jowangyu75@dgsw.hs.kr>
2024-04-08 20:53:20 +0900

    add aws gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,7 @@ dependencies {
     implementation(libs.connector.mysql)
     implementation(libs.redis)
     implementation(libs.bundles.jackson)
+    implementation(libs.spring.aws)
     testImplementation(libs.bundles.test)
 }
 

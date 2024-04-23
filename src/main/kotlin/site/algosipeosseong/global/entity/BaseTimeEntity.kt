e773649d2cd94256817ba760d2f7b287b58daf49
pea06 <jowangyu75@dgsw.hs.kr>
2024-04-23 21:35:33 +0900

    update base time entity variable val to var

--- src/main/kotlin/site/algosipeosseong/global/entity/BaseTimeEntity.kt
+++ src/main/kotlin/site/algosipeosseong/global/entity/BaseTimeEntity.kt
@@ -2,19 +2,19 @@ package site.algosipeosseong.global.entity
 
 import jakarta.persistence.Column
 import jakarta.persistence.EntityListeners
+import jakarta.persistence.MappedSuperclass
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.LocalDateTime
 
+@MappedSuperclass
 @EntityListeners(AuditingEntityListener::class)
 abstract class BaseTimeEntity(
     @CreatedDate
     @Column(updatable = false)
-    protected val createdDate: LocalDateTime? = null,
+    protected var createdDate: LocalDateTime? = null,
 
     @LastModifiedDate
-    protected val updateDate: LocalDateTime? = null
-) {
-
-}
+    protected var updateDate: LocalDateTime? = null
+)

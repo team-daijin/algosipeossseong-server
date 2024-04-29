a922664b25e87da9aea386a557ed107a55271c16
pea06 <jowangyu75@dgsw.hs.kr>
2024-04-29 20:55:54 +0900

    add valid exception handler

--- src/main/kotlin/site/algosipeosseong/global/exception/ExceptionAdvice.kt
+++ src/main/kotlin/site/algosipeosseong/global/exception/ExceptionAdvice.kt
@@ -1,6 +1,8 @@
 package site.algosipeosseong.global.exception
 
 import org.springframework.http.ResponseEntity
+import org.springframework.validation.BindException
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import site.algosipeosseong.global.exception.error.ErrorResponse
@@ -8,9 +10,19 @@ import site.algosipeosseong.global.exception.error.ErrorResponse
 @RestControllerAdvice
 class ExceptionAdvice {
     @ExceptionHandler(AlgosipeosseongException::class)
-    fun businessException(e: AlgosipeosseongException): ResponseEntity<ErrorResponse> {
-        return ResponseEntity
-            .status(e.errorProperty.status())
-            .body(ErrorResponse.of(e.errorProperty))
-    }
+    fun businessException(
+        e: AlgosipeosseongException
+    ) = ErrorResponse.of(e.errorProperty)
+
+    @ExceptionHandler(BindException::class)
+    fun bindException(
+        e: BindException
+    ) = ErrorResponse.of(e.bindingResult)
+
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    fun methodArgumentNotValidException(
+        e: MethodArgumentNotValidException
+    ) = ErrorResponse.of(e)
+
+
 }
--- src/main/kotlin/site/algosipeosseong/global/exception/error/ErrorResponse.kt
+++ src/main/kotlin/site/algosipeosseong/global/exception/error/ErrorResponse.kt
@@ -1,12 +1,30 @@
 package site.algosipeosseong.global.exception.error
 
+import org.springframework.boot.context.properties.bind.BindResult
+import org.springframework.validation.BindingResult
+
 data class ErrorResponse(
     val status: Int,
-    val message: String
+    val message: Any
 ) {
     companion object {
         fun of(errorProperty: ErrorProperty): ErrorResponse {
             return ErrorResponse(status = errorProperty.status().value(), message = errorProperty.message())
         }
+
+        fun of(e: BindingResult): ErrorResponse {
+            val errorMap = HashMap<String, String?>()
+
+            for (error in e.fieldErrors) {
+                errorMap[error.field] = error.defaultMessage
+            }
+
+            return ErrorResponse(
+                status = 400,
+                message = errorMap
+            )
+        }
+
+
     }
 }

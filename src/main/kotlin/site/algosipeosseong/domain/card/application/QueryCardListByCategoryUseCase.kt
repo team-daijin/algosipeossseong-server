8a4b696b4f073eedc4ecfb68cb9f8bdcc8d33bc6
pea06 <jowangyu75@dgsw.hs.kr>
2024-05-08 10:38:06 +0900

    update card simple response

--- src/main/kotlin/site/algosipeosseong/domain/card/application/QueryCardListByCategoryUseCase.kt
+++ src/main/kotlin/site/algosipeosseong/domain/card/application/QueryCardListByCategoryUseCase.kt
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Component
 import site.algosipeosseong.domain.card.application.dto.response.CardSimpleResponse
 import site.algosipeosseong.domain.card.domain.constant.Category
 import site.algosipeosseong.domain.card.infrastructure.CardRepository
+import site.algosipeosseong.global.util.DateUtil
 
 @Component
 class QueryCardListByCategoryUseCase(
@@ -14,13 +15,17 @@ class QueryCardListByCategoryUseCase(
     ): List<CardSimpleResponse> {
         val cards = cardRepository.findByCategoryOrderByCreatedDateDesc(category)
 
+
         return cards.stream().map {
             CardSimpleResponse(
                 id = it.id!!,
                 thumbnail = it.thumbnail,
                 category = it.category.value,
-                title = it.title
-            )
+                expert = it.expert.name,
+                date = DateUtil.dateToString(it.createdDate!!),
+                title = it.title,
+                subTitle = it.subTitle
+                )
         }.toList()
     }
 }

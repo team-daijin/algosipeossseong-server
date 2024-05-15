c04958e99de8e121491eda56820c414a5ad31c7a
pea06 <jowangyu75@dgsw.hs.kr>
2024-05-15 15:58:54 +0900

    add rest client config

---
+++ src/main/kotlin/site/algosipeosseong/global/config/RestClientConfiguration.kt
@@ -0,0 +1,13 @@
+package site.algosipeosseong.global.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.client.RestClient
+
+@Configuration
+class RestClientConfiguration {
+    @Bean
+    fun restClient(builder: RestClient.Builder): RestClient {
+        return builder.build()
+    }
+}

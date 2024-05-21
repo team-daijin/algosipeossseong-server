1cbc689bb4eaf656c591db0d73efc66f79f3ae47
pea06 <jowangyu75@dgsw.hs.kr>
2024-05-21 10:18:36 +0900

    add register banner request end date past validation

--- src/main/kotlin/site/algosipeosseong/domain/banner/application/dto/RegisterBannerRequest.kt
+++ src/main/kotlin/site/algosipeosseong/domain/banner/application/dto/RegisterBannerRequest.kt
@@ -1,5 +1,6 @@
 package site.algosipeosseong.domain.banner.application.dto
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotNull
 import jakarta.validation.constraints.Past
@@ -8,8 +9,11 @@ import java.time.LocalDate
 data class RegisterBannerRequest(
     @field:NotBlank(message = "필수 값입니다")
     val image: String,
+
     val redirectUrl: String,
+
     @field:NotNull(message = "필수 값입니다")
-    @Past(message = "종료 날짜는 과거일 수 없습니다.")
+    @field:Past(message = "종료 날짜는 과거일 수 없습니다.")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     val endDate: LocalDate
 )
